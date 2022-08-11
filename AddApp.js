5ef97bc29c48288a2304b5436d5662d4018a8288
Noam David <107786627+08-Noam-David@users.noreply.github.com>
2022-08-11 13:36:45 +0300

    Add validation

--- AddApp.js
+++ AddApp.js
@@ -15,7 +15,36 @@ const getNextId = () => {
 };
 
 const handlePublishForm = (event) => {
-    event.preventDefault();
+  event.preventDefault();
+  const form = event.target;
+  const alert = document.querySelector('#invalidAlert');
+
+  if(form.reportValidity()) {
+    if (form.classList.contains('was-validated')) {
+      form.classList.remove('was-validated');
+    }
+
+    alert.remove();
+
+    addItemToTheList({
+      id: getNextId(),
+      imageUrl: form.elements['ImageUrl'],
+      name: form.elements['name'],
+      price: form.elements['price'],
+      desc: form.elements['description'],
+      companyName: form.elements['companyName']
+    });
+
+    location.assign('mainPage.html');
+  } else {
+    if (!form.classList.contains('was-validated')) {
+      form.classList.add('was-validated');
+    }
+
+    if(alert.classList.contains('invisible')) {
+      alert.classList.remove('invisible');
+    }
+  }
 };
 
 document.addEventListener('DOMContentLoaded', () => {
