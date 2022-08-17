bd87066137ed8c23e4a7ec713eaaf761264855c5
Noam David <107786627+08-Noam-David@users.noreply.github.com>
2022-08-17 12:23:02 +0300

    Fix app list

--- main.js
+++ main.js
@@ -7,71 +7,38 @@ const getData = () => {
   return JSON.parse(localStorage.getItem('applications'));
 };
 
-const renderAppCardLogo = (app) => {
-  const appLogo = document.createElement('img');
-  appLogo.src = `images/${app.id}/${app.imageUrl}`;
-  appLogo.alt = `Logo of ${app.name}`;
-  appLogo.style.height = '160px';
-  appLogo.addEventListener('error', () => {
-    appLogo.src = 'images/Help.png';
-    appLogo.alt = 'Unkown logo';
-  });
-
-  const appLogoContainer = document.createElement('div');
-  appLogoContainer.classList.add('col-md-4');
-
-  appLogoContainer.appendChild(appLogo);
-
-  return appLogoContainer;
-};
-
-const renderAppCardBody = (app) => {
-  const appName = document.createElement('h5');
-  appName.classList.add('card-title');
-  appName.textContent = app.name;
-
-  const appDetails = document.createElement('p');
-  appDetails.classList.add('card-text');
-  appDetails.innerText = `
-  ${app.desc}
-  Price: ${app.price}
-  Company name: ${app.companyName}
-  `;
-
-  const cardBody = document.createElement('div');
-  cardBody.classList.add('card-body');
-
-  const cardBodyContainer = document.createElement('div');
-  cardBodyContainer.classList.add('col-md-8');
-
-  cardBody.appendChild(appName);
-  cardBody.appendChild(appDetails);
-  cardBodyContainer.appendChild(cardBody);
-
-  return cardBodyContainer;
-};
-
 const createAppCard = (app) => {
-  const appLogo = renderAppCardLogo(app);
-  const appDetails = renderAppCardBody(app);
-
-  const appCardConatiner = document.createElement('div');
-  appCardConatiner.classList.add('row', 'g-0');
-  appCardConatiner.appendChild(appLogo);
-  appCardConatiner.appendChild(appDetails);
-
-  const appCard = document.createElement('article');
-  appCard.classList.add('card', 'mt-2', 'border-0');
-  appCard.appendChild(appCardConatiner);
-
-  return appCard;
+  return `
+  <div class="card mb-3 border border-0" style="max-width: 540px;">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img 
+          src="images/${app.id}/${app.imageUrl}"
+          class="img-fluid app-logo"
+          alt="Logo of ${app.name}"
+          style="height: 160px;"
+          onerror="this.src = 'images/Help.png';"
+        />
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">${app.name}</h5>
+          <p class="card-text">
+            ${app.desc}<br />
+            Price: ${app.price}<br />
+            Company name: ${app.companyName}<br />
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
 };
 
 const renderAppData = (arr) => {
   const appCardList = document.querySelector('main');
 
-  appCardList.innerText = '';
-  arr.map(createAppCard).forEach((card) => appCardList.appendChild(card));
+  appCardList.innerHTML = arr.map(createAppCard).join('');
 };
 
 const handleSearch = (event) => {
